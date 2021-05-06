335079735aa089ada77ab577c0875d678c3c0980
jaden <jonghaha7@gmail.com>
2021-05-07 00:51:02 +0900

    [GH-23] DaumCrawlerService 리팩토링

--- webtoon-core/src/main/kotlin/com/depromeet/webtoon/core/crawl/daum/DaumCrawlerFetchService.kt
+++ webtoon-core/src/main/kotlin/com/depromeet/webtoon/core/crawl/daum/DaumCrawlerFetchService.kt
@@ -1,5 +1,6 @@
 package com.depromeet.webtoon.core.crawl.daum
 
+import com.depromeet.webtoon.core.crawl.daum.dto.webtoondetail.DaumWebtoonDetailCrawlResult
 import com.depromeet.webtoon.core.crawl.daum.dto.webtoonlist.DaumWebtoonCrawlResult
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
@@ -17,23 +18,25 @@ class DaumCrawlerFetchService {
         .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
         .build()
 
-
-
-
-
+    // 웹툰 상세 정보 ( genre 정보를 위해 필요 )
+    fun crawlWebtoonDetail(nickname: String): DaumWebtoonDetailCrawlResult? {
+        return client.get()
+            .uri("data/mobile/webtoon/view?nickname=$nickname").retrieve()
+            .bodyToMono(DaumWebtoonDetailCrawlResult::class.java).block()
+    }
 
     // 오늘 업데이트 된 웹툰 nicknames 가져오기
-    private fun getTodayUpdatedNicknames(): List<String> {
-        val currentWeekDay = getWeekday();
+     fun getTodayUpdatedNicknames(): List<String> {
+        val currentWeekDay = getWeekday()
         val currentTime = System.currentTimeMillis()
         val result = getCrawlResultOfTodayUpdated(client, currentWeekDay, currentTime)
         return result!!.data.webtoons.filterNot { it.ageGrade == 19 }.map { it.nickname }
     }
 
+    // 요일 반환
     private fun getWeekday(): String {
         val weekDay = arrayOf("sun", "mon", "tue", "wed", "thu", "fri", "sat")
-
-        return weekDay[Calendar.getInstance().get(Calendar.DAY_OF_WEEK-1)]
+        return weekDay[Calendar.getInstance().get(Calendar.DAY_OF_WEEK)-1]
     }
 
     // 다음웹툰 업데이트 순 크롤링
@@ -48,7 +51,7 @@ class DaumCrawlerFetchService {
     }
 
     // 인기순 웹툰의 nickname 반환 ( for webtoonDetail crawl )
-    private fun getPopularNicknames(): ArrayList<String> {
+     fun getPopularNicknames(): ArrayList<String> {
         val popularWebtoonNicknames = ArrayList<String>()
         val currentTime = System.currentTimeMillis()
         // 인기 목록 1 ~ 50
@@ -65,7 +68,6 @@ class DaumCrawlerFetchService {
         return popularWebtoonNicknames
     }
 
-
     // 다음웹툰 인기 순 크롤링
     private fun crawlPopularWebtoons(
         pageNum: Int,
@@ -75,7 +77,6 @@ class DaumCrawlerFetchService {
             .uri("data/mobile/webtoon?sort=view&page_size=50&page_no=$pageNum&$currentTime").retrieve()
             .bodyToMono(DaumWebtoonCrawlResult::class.java).block()
     }
-
 
     companion object {
         const val DAUM_WEBTOON_ROOT_URL = "http://m.webtoon.daum.net"
--- webtoon-core/src/main/kotlin/com/depromeet/webtoon/core/crawl/daum/DaumCrawlerService.kt
+++ webtoon-core/src/main/kotlin/com/depromeet/webtoon/core/crawl/daum/DaumCrawlerService.kt
@@ -2,149 +2,64 @@ package com.depromeet.webtoon.core.crawl.daum
 
 import com.depromeet.webtoon.core.application.imports.WebtoonImportService
 import com.depromeet.webtoon.core.application.imports.dto.WebtoonImportRequest
-import com.depromeet.webtoon.core.crawl.daum.dto.webtoondetail.DaumWebtoonDetailCrawlResult
 import com.depromeet.webtoon.core.crawl.daum.dto.webtoondetail.WebtoonWeeks
-import com.depromeet.webtoon.core.crawl.daum.dto.webtoonlist.DaumWebtoonCrawlResult
 import com.depromeet.webtoon.core.type.WebtoonSite
 import com.depromeet.webtoon.core.type.WeekDay
 import org.slf4j.LoggerFactory
-import org.springframework.http.HttpHeaders
-import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
-import org.springframework.web.reactive.function.client.WebClient
-import java.time.DayOfWeek
-import java.time.Instant
-import java.time.LocalDate
 
 // API 서비스 호출 + 정제
 @Service
 class DaumCrawlerService(
-    val webtoonImportService: WebtoonImportService
+    val webtoonImportService: WebtoonImportService,
+    val fetchService: DaumCrawlerFetchService
 ) {
 
-    private val client: WebClient = WebClient.builder()
-        .baseUrl("http://m.webtoon.daum.net")
-        .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-        .build()
+    fun updateDaumWebtoons(){
+        val updatedWebtoons = crawlSortedWebtoonNicknames()
+            .map { fetchService.crawlWebtoonDetail(it) }
 
-    private val currentTime = Instant.now().toEpochMilli()
-    private val currentWeekDay = LocalDate.now().dayOfWeek.name.toLowerCase().substring(0, 3)
-
-    fun updateDaumWebtoons2(){
-
-    }
-
-    fun updateDaumWebtoons() {
-        val todayWebtoonNicknamesSortedAsPopularity = getSortedNicknamesByPopularity()
-        val webtoonImportRequests = getWebtoonImportRequests(todayWebtoonNicknamesSortedAsPopularity)
-
-        val listIterator = webtoonImportRequests.listIterator()
-        while (listIterator.hasNext()) {
-            val importRequest = listIterator.next()
-            webtoonImportService.importWebtoon(importRequest)
-            log.info("[DaumCrawlerService] - webtoon import. webtoon title : ${importRequest.title}")
-        }
-    }
-
-    private fun getWebtoonImportRequests(nicknames: List<String>): List<WebtoonImportRequest> {
-        val webtoonImportRequests = ArrayList<WebtoonImportRequest>()
-        fun crawlResultToWebtoonImportRequest(
-            crawlResult: DaumWebtoonDetailCrawlResult,
-            popular: Int
-        ): WebtoonImportRequest {
-
-            fun setDayOfWeek(list: List<WebtoonWeeks>): List<WeekDay> {
-                val dayOfWeeks = ArrayList<WeekDay>()
-                list.forEach {
-                    if (it.weekDay == "mon") dayOfWeeks.add(WeekDay.MON)
-                    if (it.weekDay == "tue") dayOfWeeks.add(WeekDay.TUE)
-                    if (it.weekDay == "wed") dayOfWeeks.add(WeekDay.WED)
-                    if (it.weekDay == "thu") dayOfWeeks.add(WeekDay.THU)
-                    if (it.weekDay == "fri") dayOfWeeks.add(WeekDay.FRI)
-                    if (it.weekDay == "sat") dayOfWeeks.add(WeekDay.SAT)
-                    if (it.weekDay == "sun") dayOfWeeks.add(WeekDay.SUN)
-                }
-                return dayOfWeeks
-            }
-
-            return WebtoonImportRequest(
-                title = crawlResult.data.webtoon.title,
-                dayOfWeeks = setDayOfWeek(crawlResult.data.webtoon.webtoonWeeks),
-                authors = crawlResult.data.webtoon.cartoon.artists.map { it.name }.distinct(),
-                site = WebtoonSite.DAUM,
-                genres = crawlResult.data.webtoon.cartoon.genres.map { it.name },
-                score = crawlResult.data.webtoon.averageScore,
-                popular = popular,
-                thumbnailImage = crawlResult.data.webtoon.thumbnailImage2.url,
-                url = "TODO",
+        val webtoonImportRequests = updatedWebtoons.mapIndexed{ idx, crawled ->
+            WebtoonImportRequest(
+                crawled!!.data.webtoon.title,
+                "url",
+                crawled.data.webtoon.thumbnailImage2.url,
+                setDayOfWeek(crawled.data.webtoon.webtoonWeeks),
+                crawled.data.webtoon.cartoon.artists.map { it.name }.distinct(),
+                WebtoonSite.DAUM,
+                crawled.data.webtoon.cartoon.genres.map { it.name },
+                crawled.data.webtoon.averageScore,
+                idx+1
             )
         }
 
-        val listIterator = nicknames.listIterator()
+        webtoonImportRequests.map { webtoonImportService.importWebtoon(it) }
 
-        while (listIterator.hasNext()) {
-            val crawlResult = client.get()
-                .uri("data/mobile/webtoon/view?nickname=${listIterator.next()}").retrieve()
-                .bodyToMono(DaumWebtoonDetailCrawlResult::class.java).block()
+        log.info("[DaumCrawlerService] - updateDaumWebtoons() complete")
+    }
 
-            val crawlResultToWebtoonImportRequest =
-                crawlResultToWebtoonImportRequest(crawlResult!!, listIterator.nextIndex())
+    fun crawlSortedWebtoonNicknames() : List<String>{
+        val popularNicknames = fetchService.getPopularNicknames()
+        val updatedNicknames = fetchService.getTodayUpdatedNicknames()
 
-            webtoonImportRequests.add(crawlResultToWebtoonImportRequest)
+        val popular = popularNicknames.filter { it in updatedNicknames }
+        val unpopular = updatedNicknames.filterNot { it in popularNicknames }
+
+        return popular + unpopular
+    }
+
+    fun setDayOfWeek(list: List<WebtoonWeeks>): List<WeekDay> {
+        val dayOfWeeks = ArrayList<WeekDay>()
+        list.forEach {
+            if (it.weekDay == "mon") dayOfWeeks.add(WeekDay.MON)
+            if (it.weekDay == "tue") dayOfWeeks.add(WeekDay.TUE)
+            if (it.weekDay == "wed") dayOfWeeks.add(WeekDay.WED)
+            if (it.weekDay == "thu") dayOfWeeks.add(WeekDay.THU)
+            if (it.weekDay == "fri") dayOfWeeks.add(WeekDay.FRI)
+            if (it.weekDay == "sat") dayOfWeeks.add(WeekDay.SAT)
+            if (it.weekDay == "sun") dayOfWeeks.add(WeekDay.SUN)
         }
-        return webtoonImportRequests
-    }
-
-    fun getSortedNicknamesByPopularity(): List<String> {
-        val popularNicknames = getPopularNicknames()
-        val todayNicknames = getTodayUpdatedNicknames()
-
-        val todayPopulars = popularNicknames.filter { it in todayNicknames }
-        val todayUnPopulars = todayNicknames.filterNot { it in popularNicknames }
-
-        return todayPopulars + todayUnPopulars
-    }
-
-    private fun getPopularNicknames(): ArrayList<String> {
-        val popularWebtoonNicknames = ArrayList<String>()
-        // 인기 목록 1 ~ 50
-        popularWebtoonNicknames.addAll(
-            getCrawlResultOfPopular(1, client, currentTime)!!
-                .data.webtoons.filterNot { it.ageGrade == 19 }.map { it.nickname }
-        )
-
-        // 인기 목록 51 ~ 100
-        popularWebtoonNicknames.addAll(
-            getCrawlResultOfPopular(2, client, currentTime)!!
-                .data.webtoons.filterNot { it.ageGrade == 19 }.map { it.nickname }
-        )
-        return popularWebtoonNicknames
-    }
-
-    // 오늘 업데이트 된 웹툰 nicknames 가져오기
-    private fun getTodayUpdatedNicknames(): List<String> {
-        val result = getCrawlResultOfTodayUpdated(client, currentWeekDay, currentTime)
-        return result!!.data.webtoons.filterNot { it.ageGrade == 19 }.map { it.nickname }
-    }
-
-    private fun getCrawlResultOfTodayUpdated(
-        client: WebClient,
-        currentWeekday: String,
-        currentTime: Long
-    ): DaumWebtoonCrawlResult? {
-        return client.get()
-            .uri("data/mobile/webtoon?sort=update&page_size=50&week=$currentWeekday&page_no=1&$currentTime").retrieve()
-            .bodyToMono(DaumWebtoonCrawlResult::class.java).block()
-    }
-
-    private fun getCrawlResultOfPopular(
-        pageNum: Int,
-        client: WebClient,
-        currentTime: Long
-    ): DaumWebtoonCrawlResult? {
-        return client.get()
-            .uri("data/mobile/webtoon?sort=view&page_size=50&page_no=$pageNum&$currentTime").retrieve()
-            .bodyToMono(DaumWebtoonCrawlResult::class.java).block()
+        return dayOfWeeks
     }
 
     companion object {
