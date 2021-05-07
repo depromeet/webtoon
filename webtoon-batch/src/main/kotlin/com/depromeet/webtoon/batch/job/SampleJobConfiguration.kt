6da8c474d7911a9ae44e98c6cebc16e4ee418608
jaden <jonghaha7@gmail.com>
2021-05-07 19:12:41 +0900

    webtoonbatch 테스트

--- webtoon-batch/src/main/kotlin/com/depromeet/webtoon/batch/job/SampleJobConfiguration.kt
+++ webtoon-batch/src/main/kotlin/com/depromeet/webtoon/batch/job/SampleJobConfiguration.kt
@@ -2,6 +2,7 @@ package com.depromeet.webtoon.batch.job
 
 import com.depromeet.webtoon.batch.job.SampleJobConfiguration.Companion.SAMPLE_JOB_NAME
 import com.depromeet.webtoon.batch.support.ParamCleanRunIdIncrementer
+import com.depromeet.webtoon.core.crawl.daum.DaumCrawlerService
 import org.slf4j.LoggerFactory
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.Step
@@ -18,7 +19,8 @@ import org.springframework.context.annotation.Configuration
 @ConditionalOnProperty(name = ["spring.batch.job.names"], havingValue = SAMPLE_JOB_NAME)
 class SampleJobConfiguration(
     val jobBuilderFactory: JobBuilderFactory,
-    val stepBuilderFactory: StepBuilderFactory
+    val stepBuilderFactory: StepBuilderFactory,
+    val crawlerService: DaumCrawlerService
 ) {
     private val log = LoggerFactory.getLogger(SampleJobConfiguration::class.java)
 
@@ -36,6 +38,7 @@ class SampleJobConfiguration(
         return stepBuilderFactory.get("sampleStep")
             .tasklet { contribution, chunckContext ->
                 log.info("helloWorld $requestDate")
+                crawlerService.updateDaumWebtoons()
                 RepeatStatus.FINISHED
             }
             .build()
