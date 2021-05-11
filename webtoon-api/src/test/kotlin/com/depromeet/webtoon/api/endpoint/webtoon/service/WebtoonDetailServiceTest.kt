daac23798fd24940343833876f69d6163ab18691
jaden <jonghaha7@gmail.com>
2021-05-11 16:50:50 +0900

    [GH-31] Webtoon_Detail_API

---
+++ webtoon-api/src/test/kotlin/com/depromeet/webtoon/api/endpoint/webtoon/service/WebtoonDetailServiceTest.kt
@@ -0,0 +1,8 @@
+package com.depromeet.webtoon.api.endpoint.webtoon.service
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class WebtoonDetailServiceTest : FunSpec({
+
+
+})
