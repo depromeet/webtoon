2f83c8b96a3c96d37bd86776d3638831124bd7e1
devsejong <sejong.park@woowahan.com>
2021-05-14 21:31:43 +0900

    [GH-00] flyway 플러그인으로 실행하도록 개선

--- webtoon-core/build.gradle.kts
+++ webtoon-core/build.gradle.kts
@@ -1,3 +1,7 @@
+apply {
+    plugin("org.flywaydb.flyway")
+}
+
 dependencies {
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     api("org.springframework.boot:spring-boot-starter-data-jpa")
@@ -6,8 +10,6 @@ dependencies {
     // TODO 크롤러는 별도로 분리할것??!!
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.jsoup:jsoup")
-    // flyway
-    implementation("org.flywaydb:flyway-core")
 
     runtimeOnly("com.h2database:h2")
     runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
