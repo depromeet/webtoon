e36d858a783965fdd0e5200a2ad5a11f94ea2782
jaden <jonghaha7@gmail.com>
2021-05-18 00:59:10 +0900

    flyway 파일명 수정

---
+++ webtoon-core/src/main/resources/db/migration/V20210517.1__Insert_account.sql
@@ -0,0 +1,1 @@
+insert into account(device_id,nickname,created_at,modified_at) values("testDeviceId", "tester",CURRENT_TIME(),CURRENT_TIME());
--- webtoon-core/src/main/resources/db/migration/V20210517.2__insert_review.sql
+++ webtoon-core/src/main/resources/db/migration/V20210517.2__insert_review.sql
@@ -1,5 +1,3 @@
-insert into account(device_id,nickname,created_at,modified_at) values("testDeviceId", "tester",CURRENT_TIME(),CURRENT_TIME());
-
 insert into review(comment, drawing_score, story_score, account_id, webtoon_id, created_at, modified_at)
 values("재밌네요!!", 4.5, 5.0, 1, 1, CURRENT_TIME(), CURRENT_TIME());
 
