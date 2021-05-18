ffcf71d4477a0c3fc9c36e1f11e4e8224dab1513
devsejong <sejong.park@woowahan.com>
2021-05-18 22:41:55 +0900

    [GH-43] flyway 설정 다듬기

--- webtoon-core/src/main/resources/db/migration/V20210518.2__init_domain.sql
+++ webtoon-core/src/main/resources/db/migration/V20210518.2__init_domain.sql
@@ -3,16 +3,16 @@ CREATE TABLE account
     id          BIGINT       NOT NULL PRIMARY KEY AUTO_INCREMENT,
     device_id   VARCHAR(255) NOT NULL,
     nickname    VARCHAR(255) NOT NULL,
-    created_at  DATETIME(6) NOT NULL,
-    modified_at DATETIME(6) NOT NULL
+    created_at  DATETIME(6)  NOT NULL,
+    modified_at DATETIME(6)  NOT NULL
 );
 
 CREATE TABLE author
 (
     id          BIGINT       NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name        VARCHAR(255) NOT NULL,
-    created_at  DATETIME(6) NOT NULL,
-    modified_at DATETIME(6) NOT NULL
+    created_at  DATETIME(6)  NOT NULL,
+    modified_at DATETIME(6)  NOT NULL
 );
 
 CREATE TABLE webtoon
@@ -43,12 +43,19 @@ CREATE TABLE webtoon_week_day
     CONSTRAINT week_day_webtoon_id_fk FOREIGN KEY (webtoon_id) REFERENCES webtoon (id)
 );
 
+CREATE TABLE webtoon_genre
+(
+    webtoon_id BIGINT       NOT NULL,
+    genre      VARCHAR(255) NOT NULL,
+    CONSTRAINT genre_webtoon_id_fk FOREIGN KEY (webtoon_id) REFERENCES webtoon (id)
+);
+
 CREATE TABLE review
 (
-    id            BIGINT   NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    account_id    BIGINT   NOT NULL,
-    webtoon_id    BIGINT   NOT NULL,
-    comment       LONGTEXT NOT NULL,
+    id            BIGINT      NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    account_id    BIGINT      NOT NULL,
+    webtoon_id    BIGINT      NOT NULL,
+    comment       LONGTEXT    NOT NULL,
     drawing_score DOUBLE,
     story_score   DOUBLE,
     modified_at   DATETIME(6) NOT NULL,
