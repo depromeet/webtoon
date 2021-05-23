20300a17eac2daf9573eb704661a1586561ad45b
devsejong <sejong.park@woowahan.com>
2021-05-23 19:05:25 +0900

    [GH-0] URL이 잘못 매핑된 이슈 수정

--- webtoon-api/src/main/kotlin/com/depromeet/webtoon/api/endpoint/home/HomeController.kt
+++ webtoon-api/src/main/kotlin/com/depromeet/webtoon/api/endpoint/home/HomeController.kt
@@ -7,9 +7,11 @@ import com.depromeet.webtoon.core.application.api.dto.convertToWebtoonResponses
 import com.depromeet.webtoon.core.domain.webtoon.service.WebtoonService
 import com.depromeet.webtoon.core.type.WeekDay
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("api/v1")
+@RestController
+@RequestMapping("api/v1")
 class HomeController(val webtoonService: WebtoonService) {
 
     @GetMapping("/home")
