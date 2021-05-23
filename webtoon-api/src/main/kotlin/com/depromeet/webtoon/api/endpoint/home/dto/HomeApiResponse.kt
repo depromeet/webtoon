c228b1aa049f5e46c7ac4ed3a60ffd0e40e8c6c6
devsejong <sejong.park@woowahan.com>
2021-05-23 18:52:28 +0900

    [GH-55] 홈 화면 API Mock데이터 반환

---
+++ webtoon-api/src/main/kotlin/com/depromeet/webtoon/api/endpoint/home/dto/HomeApiResponse.kt
@@ -0,0 +1,19 @@
+package com.depromeet.webtoon.api.endpoint.home.dto
+
+import com.depromeet.webtoon.core.application.api.dto.WebtoonResponse
+import io.swagger.annotations.ApiModel
+import io.swagger.annotations.ApiModelProperty
+
+@ApiModel("홈화면 반환 데이터")
+data class HomeApiResponse(
+    @ApiModelProperty(value = "상단 배너")
+    val topBanner: List<TopBannerItem>,
+    @ApiModelProperty(value = "요일별 웹툰 리스트")
+    val weekdayWebtoons: List<WebtoonResponse>,
+    @ApiModelProperty(value = "인기급상승 웹툰 리스트")
+    val trendingWebttons: List<WebtoonResponse>,
+    @ApiModelProperty(value = "장르별 웹툰 리스트")
+    val genreWebtoons: List<WebtoonResponse>,
+    @ApiModelProperty(value = "몰아보기 좋은 웹툰 리스트")
+    val bingeWatchableWebtoons: List<WebtoonResponse>,
+)
