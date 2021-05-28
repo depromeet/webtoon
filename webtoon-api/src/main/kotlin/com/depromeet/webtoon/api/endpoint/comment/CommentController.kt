c6388edb8a75369a373aea497dcbaab151c527bc
jaden <jonghaha7@gmail.com>
2021-05-28 22:37:50 +0900

    [GH-54]댓글 페이징 처리

--- webtoon-api/src/main/kotlin/com/depromeet/webtoon/api/endpoint/comment/CommentController.kt
+++ webtoon-api/src/main/kotlin/com/depromeet/webtoon/api/endpoint/comment/CommentController.kt
@@ -1,6 +1,7 @@
 package com.depromeet.webtoon.api.endpoint.comment
 
 import com.depromeet.webtoon.api.endpoint.comment.dto.CommentRequest
+import com.depromeet.webtoon.api.endpoint.comment.dto.CommentsResponse
 import com.depromeet.webtoon.api.endpoint.comment.service.CommentImportService
 import com.depromeet.webtoon.api.endpoint.comment.service.CommentService
 import com.depromeet.webtoon.api.endpoint.dto.ApiResponse
@@ -8,6 +9,7 @@ import io.swagger.annotations.Api
 import io.swagger.annotations.ApiImplicitParam
 import io.swagger.annotations.ApiImplicitParams
 import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -21,6 +23,15 @@ class CommentController(
     val commentImportService: CommentImportService,
     val commentService: CommentService,
 ) {
+
+    @GetMapping("/list")
+    @ApiImplicitParams(
+        ApiImplicitParam(name = "commentId", value = "댓글id", required = false),
+        ApiImplicitParam(name = "pageSize", value = "페이지 크기", required = true)
+    )
+    fun getComments(@RequestParam commentId: Long?, pageSize: Long): ApiResponse<CommentsResponse>{
+        return commentService.getComments(commentId, pageSize)
+    }
 
     @PostMapping("/post")
     fun upsertComment(@RequestBody commentRequest: CommentRequest): ApiResponse<String> {
