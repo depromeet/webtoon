814d5c0f319a99440d3d24d3abb5f75fd6f8b11f
jaden <jonghaha7@gmail.com>
2021-06-03 12:34:22 +0900

    테스트 수정

--- webtoon-api/src/main/kotlin/com/depromeet/webtoon/api/common/CurrentUser.kt
+++ webtoon-api/src/main/kotlin/com/depromeet/webtoon/api/common/CurrentUser.kt
@@ -3,5 +3,5 @@ package com.depromeet.webtoon.api.common
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.userdetails.User
 
-class CurrentUser(username: String?, password: String?, authorities: List<GrantedAuthority>, nickname: String?) : User(username, password, authorities) {
+class CurrentUser(username: String?, password: String?, authorities: List<GrantedAuthority>, nickname: String) : User(username, password, authorities) {
 }
