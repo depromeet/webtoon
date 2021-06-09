9ebd62cbf998bf85588fb0aac3293ec52010ac32
jaden <jonghaha7@gmail.com>
2021-06-09 18:12:04 +0900

    AuthorController SwCagCgerAuthAPi 추가

--- webtoon-api/src/main/kotlin/com/depromeet/webtoon/api/endpoint/author/AuthorController.kt
+++ webtoon-api/src/main/kotlin/com/depromeet/webtoon/api/endpoint/author/AuthorController.kt
@@ -1,15 +1,19 @@
 package com.depromeet.webtoon.api.endpoint.author
 
+import com.depromeet.webtoon.api.common.annotation.SwaggerAuthApi
 import com.depromeet.webtoon.api.endpoint.author.service.AuthorRecommendService
 import com.depromeet.webtoon.api.endpoint.dto.ApiResponse
 import com.depromeet.webtoon.core.domain.author.dto.AuthorRecommendResponse
+import io.swagger.annotations.Api
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@Api("authorController")
 class AuthorController(val authorRecommendService: AuthorRecommendService) {
 
     @GetMapping("/api/v1/authors")
+    @SwaggerAuthApi
     fun recommendAuthors():ApiResponse<AuthorRecommendResponse>{
         return ApiResponse.ok(authorRecommendService.getRecommendAuthors())
     }
