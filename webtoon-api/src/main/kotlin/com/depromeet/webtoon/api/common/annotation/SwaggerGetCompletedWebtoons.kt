8fbc7aef716588a72c57bf85db26e64591896a36
jaden <jonghaha7@gmail.com>
2021-06-10 01:36:09 +0900

    [GH-88] 완결 웹툰 API

--- webtoon-api/src/main/kotlin/com/depromeet/webtoon/api/common/annotation/SwaggerGetCompletedWebtoons.kt
+++ webtoon-api/src/main/kotlin/com/depromeet/webtoon/api/common/annotation/SwaggerGetCompletedWebtoons.kt
@@ -6,7 +6,7 @@ import io.swagger.annotations.ApiImplicitParams
 @Retention(AnnotationRetention.RUNTIME)
 @ApiImplicitParams(
     ApiImplicitParam(name = "lastWebtoonId", value = "마지막 웹툰id (마지막 웹툰 이후의 웹툰 목록을 가져옵니다.)", required = false),
-    ApiImplicitParam(name = "pageSize", value = "페이지 크기", defaultValue = "20",),
+    ApiImplicitParam(name = "pageSize", value = "페이지 크기", defaultValue = "20"),
     ApiImplicitParam(
         name = "Authorization",
         value = "authorization header",
